29585d8ff457c2e64942c561e6f3418c3e478160
quandtifier <quandtifier@gmail.com>
2018-03-01 21:11:03 -0800

    Navbar not working properly with new bootstrap theme

--- ContosoUniversity/App_Start/BundleConfig.cs
+++ ContosoUniversity/App_Start/BundleConfig.cs
@@ -24,7 +24,7 @@ namespace ContosoUniversity
                       "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
+                      "~/Content/bootstrap-theme.css",
                       "~/Content/site.css"));
 
             // Set EnableOptimizations to false for debugging. For more information,
